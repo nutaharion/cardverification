b509f20fa24411960d3beea9e3a2e96e769085ce
Adrian Krystkiewicz <adriankrystkiewicz@gmail.com>
2020-05-15 19:30:47 +0200

    Updates

---
+++ precache-manifest.2a542eec0acd7c21cef5fbcb403b7afa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7bb4f84abe4ca8a5e4065850fad90b",
+    "url": "/cardverification/index.html"
+  },
+  {
+    "revision": "7de8ec9f11173d1f3f75",
+    "url": "/cardverification/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "485b53e22fa18dd23e95",
+    "url": "/cardverification/static/js/2.581580af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardverification/static/js/2.581580af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de8ec9f11173d1f3f75",
+    "url": "/cardverification/static/js/main.34c9c824.chunk.js"
+  },
+  {
+    "revision": "d1b5b49c8283c405f46e",
+    "url": "/cardverification/static/js/runtime-main.262ca953.js"
+  },
+  {
+    "revision": "e509875b41c712521a51858fda54f3ff",
+    "url": "/cardverification/static/media/american-express.e509875b.png"
+  },
+  {
+    "revision": "e5ad2efa681c28ba6f8e2100eec7dc3c",
+    "url": "/cardverification/static/media/master-card.e5ad2efa.png"
+  },
+  {
+    "revision": "6acbc86d878f0debbfc8c329f48cc604",
+    "url": "/cardverification/static/media/visa.6acbc86d.png"
+  }
+]);
